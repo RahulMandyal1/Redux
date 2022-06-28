0b362c1a25aee492f1675d2f6e8f349912b76ef6
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-06-28 19:29:13 +0530

    middleware in  the redux to add custom functionality

---
+++ CoreReduxConcept/middlewareinredux.js
@@ -0,0 +1,59 @@
+const Redux = require("redux");
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
+const applyMiddelware = Redux.applyMiddleware;
+
+// function that will call once inside dispath will dispath   the action mutate the state
+const Increment = () => {
+  return {
+    type: "Increment",
+    payload: "it will increment it by one ",
+  };
+};
+// decrement value by one
+const decrement = () => {
+  return {
+    type: "decrement",
+    payload: "it will decrement the counte value by one ",
+  };
+};
+// Reset our counter value
+const reset = () => {
+  return {
+    type: "reset",
+    payload: "it will reset our counter value to zero ",
+  };
+};
+
+const counterInitialState = { counter: 0 };
+const counterReducer = (state = counterInitialState, { type, payload }) => {
+  switch (type) {
+    case "Increment":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case "reset":
+      return {
+        ...state,
+        counter: 0,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(counterReducer, applyMiddelware(logger));
+store.dispatch(Increment());
+store.dispatch(Increment());
+store.dispatch(Increment());
+store.dispatch(Increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(reset());
+store.subscribe(() => {});
