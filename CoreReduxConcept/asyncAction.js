ad50f79be7e630dbfa7ee43d71e65ee57dfb1731
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-06-29 12:37:05 +0530

    Redux thunk dispatch an asynchronous action

--- CoreReduxConcept/asyncAction.js
+++ CoreReduxConcept/asyncAction.js
@@ -1,55 +1,76 @@
 const Redux = require("redux");
+const axios = require("axios");
+const thunk = require("redux-thunk").default;
+const applyMiddelware = Redux.applyMiddleware;
 
 const initialState = {
   loading: false,
-  user: null,
+  users: null,
   error: "",
 };
 
-const FETCH_USER_REQUEST = "FETCH_USER_REQUEST";
-const FETCH_USER_SUCESS = "FETCH_USER_SUCESS";
-const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCESS = "FETCH_USERS_SUCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
 
 const fetchUserRequest = () => {
   return {
-    type: FETCH_USER_REQUEST,
+    type: FETCH_USERS_REQUEST,
   };
 };
 
-const fetchUserSucess = () => {
+const fetchUserSucess = (userData) => {
   return {
-    type: FETCH_USER_SUCESS,
-    payload: "here comes the user data if the user is found",
+    type: FETCH_USERS_SUCESS,
+    payload: userData,
   };
 };
 
-const fetchUserError = () => {
+const fetchUserError = (errorMessage) => {
   return {
-    type: FETCH_USER_FAILURE,
-    payload: "some kind of error if  we get",
+    type: FETCH_USERS_FAILURE,
+    payload: errorMessage,
   };
 };
 
 const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case FETCH_USER_REQUEST:
+    case FETCH_USERS_REQUEST:
       return {
         ...state,
         loading: true,
       };
-    case FETCH_USER_SUCESS:
+    case FETCH_USERS_SUCESS:
       return {
         loading: false,
-        user: payload,
+        users: payload,
         error: "",
       };
-    case FETCH_USER_FAILURE:
+    case FETCH_USERS_FAILURE:
       return {
-        ...state,
         loading: false,
+        users: null,
         error: payload,
       };
   }
 };
 
-const store = Redux.createStore(reducer);
+const fetchUser = () => {
+  return function (dispatch) {
+    dispatch(fetchUserRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(({ data }) => {
+        // if  we recieve any kind of {data} then we have to dispatch an action from here
+        const users = data.map((user) => user.username);
+        dispatch(fetchUserSucess(users));
+      })
+      .catch((error) => {
+        dispatch(fetchUserError(error.message));
+      });
+  };
+};
+
+const store = Redux.createStore(reducer, applyMiddelware(thunk));
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(fetchUser());
