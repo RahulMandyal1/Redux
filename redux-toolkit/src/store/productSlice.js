0469a309ef0615e96134714fdd962f474949f1f1
Rahul Thakur <rahulmandyal079@gmail.com>
2022-12-02 10:59:22 +0530

    asynchronous action with createAsyncThunk fun and extra reducers

--- redux-toolkit/src/store/productSlice.js
+++ redux-toolkit/src/store/productSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const STATUSES = Object.freeze({
   IDLE: "idle",
@@ -13,29 +13,55 @@ const productSlice = createSlice({
     status: STATUSES.IDLE,
   },
   reducers: {
-    setProducts(state, action) {
+    // setProducts(state, action) {
+    //   state.data = action.payload;
+    // },
+    // setStatus(state, action) {
+    //   state.status = action.payload;
+    // },
+  },
+  extraReducers: (builder) => {
+    // it works like switch case statement
+    builder.addCase(fetchProductsData.pending, (state, action) => {
+      state.status = STATUSES.LOADING;
+    });
+
+    // when data is fetch and promise is fullfilled
+    builder.addCase(fetchProductsData.fulfilled, (state, action) => {
+      state.status = STATUSES.IDLE;
       state.data = action.payload;
-    },
-    setStatus(state, action) {
-      state.status = action.payload;
-    },
+    });
+
+    //promise is rejected data is not fetched or if there is an error
+    builder.addCase(fetchProductsData.rejected, (state, action) => {
+      state.status = STATUSES.ERROR;
+    });
   },
 });
 
-export const { setProducts, setStatus } = productSlice.actions;
+// export const { setProducts, setStatus } = productSlice.actions;
 //export redcuer from here
 export default productSlice.reducer;
 
+// create Async Thunk middleware function
+export const fetchProductsData = createAsyncThunk(
+  "products/fetch",
+  async () => {
+    const data = await fetch(`https://fakestoreapi.com/products`);
+    return await data.json();
+  }
+);
+
 //get the products data  with the help of thunk  || thunk is inbuild  in
 //redux toolkit so we do not need to add thunk middleware externally
-export const fetchProductsData = () => async (dispatch) => {
-  try {
-    dispatch(setStatus(STATUSES.LOADING));
-    const data = await fetch(`https://fakestoreapi.com/products`);
-    const jsonData = await data.json();
-    dispatch(setProducts(jsonData));
-    dispatch(setStatus(STATUSES.IDLE));
-  } catch (err) {
-    dispatch(setStatus(STATUSES.ERROR));
-  }
-};
+// export const fetchProductsData = () => async (dispatch) => {
+//   try {
+//     dispatch(setStatus(STATUSES.LOADING));
+//     const data = await fetch(`https://fakestoreapi.com/products`);
+//     const jsonData = await data.json();
+//     dispatch(setProducts(jsonData));
+//     dispatch(setStatus(STATUSES.IDLE));
+//   } catch (err) {
+//     dispatch(setStatus(STATUSES.ERROR));
+//   }
+// };
